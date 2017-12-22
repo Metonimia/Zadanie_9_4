6bf7b5fe5de67fb0fd360c060bb4dd20772c8d2a
Zuzanna <zuzanna.zuzewicz@gmail.com>
2017-12-22 10:58:36 +0200

    Updated scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -23,6 +23,24 @@
 
 drawTree(5);*/
 
+// function drawTree(treeHeight) {
+
+
+// 	for (var i = 1; i <= treeHeight; i++) {
+// 		var star = '';
+
+// 		for (var j = 1; j <= i; j++) {
+// 			star += '*';
+// 		}
+
+// 		console.log(star);
+// 	}
+// }
+
+// drawTree(1);
+// drawTree(2);
+// drawTree(5);
+
 
 function drawTree(treeHeight) {
 
@@ -30,18 +48,20 @@ function drawTree(treeHeight) {
 	for (var i = 1; i <= treeHeight; i++) {
 		var star = '';
 
-		for (var j = 1; j <= i; j++) {
-			star += '*';
-		}
+		for (var j = i; i >= (treeHeight - 1); j++) {
+  			star += ' ';
+
+			for (var h = j; h <= i; h++) {
+				star += '*';
+
+
+
+			}
 
 		console.log(star);
+		}
 	}
+
 }
 
-drawTree(1);
-drawTree(2);
 drawTree(5);
-
-// Bardzo się starałam coś wykombinować z tymi spacjami, ale bezowocnie do tej pory. Postaram się do tego wrócić albo może możemy omówić to w poniedziałek?
-
-
